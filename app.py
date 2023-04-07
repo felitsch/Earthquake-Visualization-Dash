391e0ae3cc70783d59c181ff411523c1e3ac9892
felitsch <107850501+felitsch@users.noreply.github.com>
2023-04-07 14:05:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -41,5 +41,10 @@ app.layout = layouts.create_dashboard_layout()
 callbacks.register_callbacks(app)
 
 # Run app
+#if __name__ == '__main__':
+#    app.run_server(debug=True)
+   
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    server.run(debug=True)
+
